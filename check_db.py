3e4418480a3ef8bd3e2b808949d01b486972c76b
V44nxx <murciacorredoremerson@gmail.com>
2026-07-24 16:28:07 -0500

    Debug variables

--- check_db.py
+++ check_db.py
@@ -1,6 +1,23 @@
+import os
 import pymysql
+from dotenv import load_dotenv
+load_dotenv()
 
-DB = dict(host='localhost', user='root', password='', database='StayHuila', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+DB_HOST = os.environ.get('DB_HOST', 'localhost')
+DB_PORT = int(os.environ.get('DB_PORT', '3306'))
+DB_USER = os.environ.get('DB_USER', 'root')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
+DB_NAME = os.environ.get('DB_NAME', 'StayHuila')
+
+DB = dict(
+    host=DB_HOST,
+    port=DB_PORT,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    database=DB_NAME,
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
 
 c = pymysql.connect(**DB)
 try:
